1e664e38aa0ddd3895549e720e953edb651ef231
won <zizpzn@gmail.com>
2022-12-29 16:00:43 +0700

    admin UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { BsFillPencilFill } from "react-icons/bs";
+import { FiShoppingBag } from "react-icons/fi";
 import { login, logout, onUserStateChange } from "../api/firebase";
 import User from "./User";
+import Button from "./ui/Button";
 
 const Navbar = () => {
   const [user, setUser] = useState();
@@ -23,15 +26,21 @@ const Navbar = () => {
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-red-400">
+        <FiShoppingBag />
         <h1>Shopee</h1>
       </Link>
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/cart">Cart</Link>
-        <Link to="/products/new" className="text-2xl"></Link>
+        {user && user.isAdmin && (
+          <Link to="/products/new" className="text-2xl">
+            <BsFillPencilFill />
+          </Link>
+        )}
+
         {user && <User user={user} />}
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
       </nav>
     </header>
   );
